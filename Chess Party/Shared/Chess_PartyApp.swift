79eea0c99b41cda95df528f8e540e4a70cb6e06e
Daniël du Preez <dupreezdaniel8@gmail.com>
2021-01-06 13:18:19 -0600

    Initial Xcode project setup.

---
+++ Chess Party/Shared/Chess_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_PartyApp.swift
+//  Shared
+//
+//  Created by Daniël du Preez on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Chess_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
